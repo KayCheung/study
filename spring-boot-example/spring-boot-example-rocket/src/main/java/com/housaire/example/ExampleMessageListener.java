f5df05f48ebedb3fa30892de0875785cce8cda2e
zhangkai <zhangkai@chinayie.com>
2019-01-24 13:31:54 +0800

    add new file

---
+++ spring-boot-example/spring-boot-example-rocket/src/main/java/com/housaire/example/ExampleMessageListener.java
@@ -0,0 +1,26 @@
+package com.housaire.example;
+
+import com.housaire.rocket.AbstractRocketMqMessageListener;
+import com.housaire.rocket.annotation.RocketMqConsumer;
+import com.housaire.rocket.annotation.RocketMqExpression;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.MessageExt;
+
+/**
+ * @author <a href="mailto:zhangkai@chinayie.com">张凯</a>
+ * @description:
+ * @date 2019/1/24 13:30
+ * @see
+ * @since 1.0.0
+ */
+@Slf4j
+@RocketMqConsumer(topic = "EXAMPLE", groupName = "TEST_CONSUMER")
+@RocketMqExpression("TEST")
+public class ExampleMessageListener extends AbstractRocketMqMessageListener<String>
+{
+    @Override
+    public void handleMessage(String data, MessageExt messageExt)
+    {
+        log.info("收到消息：{}", data);
+    }
+}
